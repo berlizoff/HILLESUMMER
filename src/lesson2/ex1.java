327dcd67d26e7f0a091f2eca20b859d33add59c9
Admin <arturberlizov@gmail.com>
2022-05-29 13:19:28 +0300

    rdy

--- src/lesson2/ex1.java
+++ src/lesson2/ex1.java
@@ -1,8 +1,13 @@
 package lesson2;
 
+
 public class ex1 {
     public static void main(String[] args) {
-        int a, b;
+        int a = (int) (Math.random()*10);
+        int b = (int) (Math.random()*10);
+        double c =(double) (a+b)/2;
+        System.out.println("Среднее арефмитичное число = "+c);
+
 
     }
 }
---
+++ src/lesson2/ex2.java
@@ -0,0 +1,21 @@
+package lesson2;
+
+import java.util.Scanner;
+
+public class ex2 {
+    public static void main(String[] args) {
+        int sum = 0;
+        double avg =0;
+        System.out.println("Введите количество чисел");
+        Scanner scanner = new Scanner(System.in);
+        int number = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < number; i++) {
+            System.out.println("Введите число ");
+            int chislo = Integer.parseInt(scanner.nextLine());
+            sum = sum+chislo;
+
+        }
+        avg = (double) sum/number;
+        System.out.println("Среднее арфметическое число "+number+ " чисел = "+ avg);
+    }
+}
