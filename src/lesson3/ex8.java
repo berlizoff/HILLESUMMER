48d261cf497d82f33b7e010c1d0a4d0678bc896c
berlizoff <59088236+berlizoff@users.noreply.github.com>
2022-05-30 11:09:24 +0300

    rdy

--- src/lesson3/ex8.java
+++ src/lesson3/ex8.java
@@ -3,7 +3,7 @@ package lesson3;
 public class ex8 {
     public static void main(String[] args) {
         int[] array = new int[20];
-        for (int i = 1; i < 20; i++) {
+        for (int i = 1; i < array.length; i++) {
             array[i]= i;
             System.out.print(array[i]+", ");
             i=i+1;
